4d546691969a03f2373f7796eb40950c92bcb349
André Dias <andre.dias@outlook.com>
2018-07-30 20:21:03 -0300

    Update HomeControllerTest.cs

--- WebApp.Tests/Controllers/HomeControllerTest.cs
+++ WebApp.Tests/Controllers/HomeControllerTest.cs
@@ -39,19 +39,6 @@ namespace WebApp.Tests.Controllers
         }
         
         [TestMethod]
-        public void AboutFail()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application descripppption page. ", result.ViewBag.Message);
-        }        
-
-        [TestMethod]
         public void Contact()
         {
             // Arrange
